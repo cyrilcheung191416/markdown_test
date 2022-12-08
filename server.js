314f3d43cdf1c607e359402179bc8c1f11285a82
cyrilcheung191416 <119845371+cyrilcheung191416@users.noreply.github.com>
2022-12-08 15:21:46 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect('mongodb://localhost/blog');
+
+// the view engine will convert the ejs code to HTML
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+
+// render will access our VIEWS folder
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: 'desc',
+  });
+  res.render('articles/index', { articles: articles });
+});
+
+app.use('/articles', articleRouter);
+
+app.listen(3000);
